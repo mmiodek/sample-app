bdf365544dc0a593c7c5b8cba0b3cbf6ea3cfd23
Mateusz Miodek <mmiodek@gmail.com>
2016-05-15 22:14:01 +0000

    adding Logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:test_user)
+	end
 	test "test error message" do
 		get login_path
 		assert_template 'sessions/new'
@@ -9,5 +13,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		assert_not flash.empty?
 		get root_path
 		assert flash.empty?, "Flash message did't disappear after rendering new page"
+	end
+
+	test "login with valid info followed by logout" do
+		get login_path
+		post login_path, session: { email: @user.email, password: 'password' }
+		assert is_logged_in?
+		assert_redirected_to @user
+		follow_redirect!
+		assert_template 'users/show'
+		assert_select "a[href=?]", login_path, count: 0
+		assert_select "a[href=?]", logout_path
+		assert_select "a[href=?]", user_path(@user)
+		delete logout_path
+		assert_not is_logged_in?
+		assert_redirected_to root_url
+		# Simulate a user clicking logout in a second window.
+    	delete logout_path
+		follow_redirect!
+		assert_select "a[href=?]", login_path
+	    assert_select "a[href=?]", logout_path,      count: 0
+	    assert_select "a[href=?]", user_path(@user), count: 0
 	end
 end
