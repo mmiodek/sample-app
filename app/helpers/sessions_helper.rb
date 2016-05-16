2cab8a2d13e21a9c38943bbf667b73f477747025
Mateusz Miodek <mmiodek@gmail.com>
2016-05-16 20:10:23 +0000

    Finished log-in-log-out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,18 +12,17 @@ module SessionsHelper
 	end
 
 # Returns user corresponding to the remember token cookie
-	def current_user
-		if (user_id = session[:user_id])
-			current_user ||= User.find_by(id: session[:user_id])
-		elsif (user_id = cookies.signed[:user_id])
-			user = User.find_by(id: user)
-			if user && user.authenticated?(cookies[:remember_token])
-				log_in user
-				@current_user = user
-			end
-		end
-		
-	end
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 
 
 # Returns true if the user is logged in, false otherwise.
