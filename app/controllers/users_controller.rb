da11e90eabcae1eee955803108b06a7adfcb230c
Mateusz Miodek <mmiodek@gmail.com>
2016-06-09 22:30:14 +0000

    Finished 9.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:edit,:update]
+  before_action :logged_in_user, only: [:index, :edit,:update]
   before_action :correct_user,   only: [:edit,:update]
 
   def show
@@ -8,11 +8,9 @@ class UsersController < ApplicationController
 
   end
 
-  # def index
-  # 	@users = User.all
-  # 	# debugger
-
-  # end
+  def index
+  	@users = User.paginate(page: params[:page])
+  end
 
   def new
   	@user = User.new
